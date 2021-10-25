5c0a8f7ddfcdfef9fd149a66cace8b9712244658
Brenda Nascimento <74272215+nascimentobrenda24@users.noreply.github.com>
2021-10-25 20:29:17 -0300

    Principal

--- ex083.py
+++ ex083.py
@@ -1,7 +1,19 @@
 #  Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar
 #  se a expressão passada está com os parênteses abertos e fechados na ordem correta.
 
+l = ["(", ")"]
+e = input('Input a value:')
 
-exp = str(input('Digite um valor: '))
-while True:
-    
+#Validating the existence of parentheses
+#First Probabily: Have parentheses
+if l.index("(") > 1 or l.index(")"):
+    print("Your equation have parentheses!!")
+
+elif l.index("(") > 1 and l.index(")") == 0:
+    print('')
+
+
+
+
+else:
+    print("Your equation haven't paretheses!!")
