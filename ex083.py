6cba84932583dbcda16e4450249f08cd0945c9bd
Brenda Nascimento <74272215+nascimentobrenda24@users.noreply.github.com>
2021-10-26 08:58:25 -0300

    Other Logic

--- ex083.py
+++ ex083.py
@@ -1,22 +1,13 @@
 #  Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar
 #  se a expressão passada está com os parênteses abertos e fechados na ordem correta.
 
-l = ["(", ")"]
-e = input('Digite um valor:')
+while True:
+    entrada = str(input('Digite a expressão: '))
 
-#Validating the existence of parentheses
+    para_esquerda = entrada.count('(')
+    para_direita = entrada.count(')')
 
-# After validate the fact with have parenthesis, we may analyze if the combination of both it is correctly
-if e.count("(") > 0 or e.count(")"):
-    print("Sua equação tem parênteses!!")
-
-# When the user digit in equation a parentheses and don't completed with other
-elif e.count("(") > 0 and e.count(")") == 1 or e.count(")") > 0 and e.count("(") == 1:
-    print('Só um parênteses detectado\nSua expressão está incompleta, logo inválida!')
-
-
-
-
-else:
-    print("Sua equação não tem parênteses!!")
-    print('Sua equação está válida!')
+    if para_esquerda == para_direita:
+        print(f'Essa expressão {entrada} é válida.')
+    else:
+        print(f'Essa expressão {entrada} é inválida.')
