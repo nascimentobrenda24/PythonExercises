2c031694f4cdcd7fd30a822aea147c6cd63a69e3
Brenda Nascimento <74272215+nascimentobrenda24@users.noreply.github.com>
2021-10-28 09:06:40 -0300

    Professor Resolution

--- ex084.py
+++ ex084.py
@@ -3,51 +3,40 @@
 # B) Uma listagem com as pessoas mais pesadas.
 # C) Uma listagem com as pessoas mais leves.
 
-persons = []
-tot_heavy = 0
-tot_lightweight = 0
-names_heavies = []
-names_lights = []
-cont = 0
+princ = []
+temp = []
+light = heavy = 0
 
 while True:
     print('='*30)
-    names = [str(input('Digite seu nome: '))]
-    weight = [float(input('Seu peso [Kg]: '))]
-    # Adding in the master list
-    persons.append(names)
-    persons.append(weight)
+    temp.append(str(input('Nome: ')))
+    temp.append(float(input('Peso[Kg]: ')))
+    # Finding the heavy and lighter
+    # In the first loop, the heavy and lighter user will the first data
+    if len(princ) == 0:
+        heavy = light = temp[1] # princ[1] = weight
+    else:
+        if temp[1] > heavy:
+            heavy = temp[1]
+        elif temp[1] < light:
+            light = temp[1]
 
-    cont += 1
-    proceed = str(input('Quer continuar? [S/N]: '))
+    # Adding the temporary database "temp" in master list "princ"
+    princ.append(temp[:]) # [:] for copy the loop currently list
+    temp.clear() # cleaning the "temp" list for clear the data in each loop
 
+    proceed = str(input('Quer continuar?[S/N]: '))
     if proceed in 'Nn':
         break
 
-    # Heavy and light weight
-    # In first time, it is the heavy and light weight
-    if cont == 0:
-        tot_heavy = tot_lightweight = persons[1]
-        #persons.append(tot_heavy)
-    else:
-        # Heavy
-        if persons[1] > tot_heavy:
-            tot_heavy = weight
-            names_heavies = names
-            # Adding in the "person" list
-            persons.append(tot_heavy)
-            persons.append(names_heavies)
-        # Light
-        elif persons[1] < tot_lightweight:
-            tot_lightweight = weight
-            names_lights = names
-            # Adding in the "person" list
-            persons.append(tot_lightweight)
-            persons.append(names_lights)
-
-
-
-print(f'Foram cadastradas {cont} pessoas')
-print(f'O maior peso foi de {tot_heavy} Kg\nO menor peso foi de {tot_lightweight} Kg')
-
-
+print('='*40)
+print(princ)
+print(f'Foram cadastradas {len(princ)} pessoas')
+print(f'O maior peso digitado foi {heavy} Kg. Peso de', end=' ')
+for p in princ:
+    if p[1] == heavy:
+        print(p[0], end='')
+print(f'\nO menor peso digitado foi {light} Kg. Peso de', end=' ')
+for p in princ:
+    if p[1] == light:
+        print(p[0], end='')
