800f09efedfb98749e309c9c2224cbfb358a8984
Brenda Nascimento <74272215+nascimentobrenda24@users.noreply.github.com>
2021-11-15 18:45:39 -0300

    Update ex087.py

--- ex087.py
+++ ex087.py
@@ -1,35 +1,43 @@
 # Aprimore o desafio anterior, mostrando no final:
-# A) A soma de todos os valores pares digitados. CHECK
+# A) A soma de todos os valores pares digitados.
 # B) A soma dos valores da terceira coluna.
 # C) O maior valor da segunda linha.
+from time import sleep
+tot_sum_pairs = three_collum_sum = bigger_value = 0
 
-matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-
-tot_sum = 0
-three_collum_sum = 0
-bigger = 0
-
+# Creating the matrix
+matrix = [[0, 0, 0],
+          [0, 0, 0],
+          [0, 0, 0]]
+# Feeding the values entered by the user into the matrix
 for l in range(0, 3):
     for c in range(0, 3):
-        matrix[l][c] = int(input(f'Digite um valor para a posição [{l}, {c}]: '))
-        # Summing every pairs values
+        matrix[l][c] = int(input(f'Digite um valor para a posição [{l},{c}]: '))
+        # Looking for even pairs values to add them
         if matrix[l][c] % 2 == 0:
-            tot_sum += matrix[l][c]
+            tot_sum_pairs += matrix[l][c]
 
-# Showing the matrix database
+print('\nAguarde alguns segundos...')
+sleep(2)
+print('A matriz gerada foi:\n')
+
+# Showing the matrix
 for l in range(0, 3):
     for c in range(0, 3):
         print(f'[{matrix[l][c]:^5}]', end='')
     print()
-
-# Summing every lines of 3 collum
+# Adding the values from the third column
 for l in range(0, 3):
     three_collum_sum += matrix[l][2]
-# Finding the bigger value
+# The biggest of the second line
 for c in range(0, 3):
-    if c == 0 or matrix[1][c] > bigger:
-        bigger = matrix[1][c]
+    if c == 0 or matrix[1][c] > bigger_value:
+        bigger_value = matrix[1][c]
 
-print(f'\nA soma de todos os valores pares desta matriz equivale a {tot_sum}')
-print(f'\nA soma dos valores da 3ª coluna são: {three_collum_sum}')
-print(f'\nO maior valor da segunda linha é o {bigger}')
+
+print()
+print('-='*20)
+print(f'A soma de todos os valores pares desta matriz equivale a {tot_sum_pairs}')
+print(f'A soma dos valores da terceira coluna resultam no valor {three_collum_sum}')
+print(f'O maior valor da segunda linha é o {bigger_value}')
+
