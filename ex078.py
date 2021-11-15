d9329ea16e39cb6fc552c772e6c027aa0f8bfd58
Brenda Nascimento <74272215+nascimentobrenda24@users.noreply.github.com>
2021-11-15 10:35:34 -0300

    Update ex078.py

--- ex078.py
+++ ex078.py
@@ -1,41 +1,33 @@
 # Faça um programa que leia 5 valores númericos e guarde-os e uma lista.
 # No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista
 
-# Empty variables to control the loopings
 list_num = []
-max = 0
-min = 0
+mx = mn = 0
 
-# First looping to validate the values in "list_num" list
 for c in range(0, 5):
     # The "append" method will be useful to add inputs of the users in the composite variable
-    list_num.append(int(input(f'Digite um valor para a posição {c}:')))
-    # Lowest and highest numbers validations
+    list_num.append(int(input(f'Digite um valor para a posição {c+1}: ')))
+
     if c == 0:
-        # In the first looping the max and minor number will be the first value provided
-        max = min = list_num[c]
     # In the second looping, we may validate with with this value is max than the last number
+        mx = mn = list_num[c]
     else:
-        if list_num[c] > max:
             max = list_num[c]
-        if list_num[c] < min:
             min = list_num[c]
+        if list_num[c] > mx:
+            mx = list_num[c]
+        if list_num[c] < mn:
+            mn = list_num[c]
 
-# Showing the databases to users
-print('='*30)
-print(f'Você digitou os valores {list_num}')
-
-print(f'O maior valor digitado foi {max} nas posições ', end='')
-
-# Looping to recognize the positions of the max and min values identifies
-# Max position
-for position, value in enumerate(list_num):
-    if value == max:
-        print(f'{position}...', end='')
-print(f'\nE o menor valor digitado foi o {min} nas posições ', end='')
+print('='*50)
+print(f'O maior valor digitado foi {mx} na posição ', end='')
+for p, v in enumerate(list_num):
+    if v == mx:
+        print(f'{p}...', end='')
 print()
-# Min position
-for position, value in enumerate(list_num):
-    if value == min:
-        print(f'{position}...', end='')
+
+print(f'\nE o menor valor digitado foi {mn} na posição ', end='')
+for p, v in enumerate(list_num):
+    if v == mn:
+        print(f'{p}...', end='')
 print()
