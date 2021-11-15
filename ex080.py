466cbe2a2933aa644c8ecccb6bd9973c1d2a2443
Brenda Nascimento <74272215+nascimentobrenda24@users.noreply.github.com>
2021-11-15 11:02:57 -0300

    Update ex080.py

--- ex080.py
+++ ex080.py
@@ -5,21 +5,22 @@
 list = []
 
 for c in range(0, 5):
-    num = int(input('Digite um valor:'))
+    num = int(input(f'\033[1;30;46mDigite um valor para a posição {c+1}:\033[m'))
+
     if num in list:
-        print('Valor duplicado! Não adicionarei')
+        print('\033[1;31mValor duplicado. Não vou adicionar!\033[m')
 
     elif num not in list:
         if c == 0 or num > list[-1]:
             list.append(num)
-            print('Adicionado na última posição com sucesso!')
+            print('\033[1;32mValor adicionado na última posição\033[m')
         else:
             pos = 0
             while pos < len(list):
                 if num <= list[pos]:
                     list.insert(pos, num)
-                    print(f'Adicionado com sucesso na {pos}ª posição')
+                    print(f'\033[1;32mValor adicionado com sucesso na {pos}ª posição')
                     break
                 pos += 1
 
-print(list)
+print(f'Sua lista é {list}')
