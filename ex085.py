ec6b8c9a3ab882825e1958ce4c0520ceca01e6ae
Brenda Nascimento <74272215+nascimentobrenda24@users.noreply.github.com>
2021-11-15 14:34:38 -0300

    Update ex085.py

--- ex085.py
+++ ex085.py
@@ -1,19 +1,18 @@
 # Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que
 # mantenha separados os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente.
 
-# Declared a list that contain others lists
-num = [[], []]
+lista = [[], []]
 
-for c in range(1, 8):
-    values = int(input(f'Digite o {c}º valor: '))
-    # Adding values in your respectives positions
-    if values % 2 == 0:
-        num[0].append(values)
+for c in range(0, 7):
+    values = int(input(f"Digite o {c+1}º número: "))
+
+    if values % 2:
+        lista[0].append(values)
     else:
-        num[1].append(values)
+        lista[1].append(values)
 
 print('-='*30)
-num[0].sort()
-num[1].sort()
-print(f'Os valores pares digitados foram: {num[0]}')
-print(f'Os valores ímpares digitados foram: {num[1]}')
+lista[0].sort()
+lista[1].sort()
+print(f'Os valores pares digitados foram: {lista[0]}'if len(lista[0]) > 1 else f'O valor par digitado foi: {lista[0]}')
+print(f'Os valores ímpares digitados foram: {lista[1]}'if len(lista[1]) > 1 else f'O valor ímpar digitado foi: {lista[1]}')
