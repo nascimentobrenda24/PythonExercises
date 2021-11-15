cdb78d2c7285266879f6df4ac1e52cbaa552c48f
Brenda Nascimento <74272215+nascimentobrenda24@users.noreply.github.com>
2021-11-15 14:54:06 -0300

    Update ex086.py

--- ex086.py
+++ ex086.py
@@ -1,21 +1,18 @@
 # Crie um programa que declare uma matriz de dimensão 3×3 e preencha com valores lidos pelo teclado. No final,
 # mostre a matriz na tela, com a formatação correta.
-# **Matrix whose have 3 lines and 3 columns
-from time import sleep
 
-# Each columns will have 3 lines
 matrix = [[0, 0, 0],
           [0, 0, 0],
           [0, 0, 0]]
-# Sweeping each lines and after columns
+# Sweeping each lines and, after, columns along of the matrix. In the final, we may computed values for each positions
 for l in range(0, 3):
     for c in range(0, 3):
-        # Adding values of the users in "matrix" varible, but in "l" variable time
         matrix[l][c] = int(input(f'Digite um valor para a posição [{l}, {c}]: '))
-print('A matriz será gerada em alguns segundos...')
-sleep(3)
+
+# Showing the matrix
 for l in range(0, 3):
     for c in range(0, 3):
         print(f'[{matrix[l][c]:^5}]', end='')
     print()
+
 
