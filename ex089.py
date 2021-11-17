783fb695473d2d2e77c1f98cdb85b2a7ef64b488
Brenda Nascimento <74272215+nascimentobrenda24@users.noreply.github.com>
2021-11-17 17:25:39 -0300

    Update ex089.py

--- ex089.py
+++ ex089.py
@@ -7,9 +7,10 @@ record = []
 while True:
     print('='*15)
     name = str(input('Nome: '))
-    grade1 = float(input('Nota 1: '))
 
+    grade1 = float(input('Nota 1: '))
     grades2 = float(input('Nota 2: '))
+
     average = (grade1 + grades2) / 2
 
     record.append([name, [grade1, grades2], average])
@@ -19,15 +20,15 @@ while True:
         break
 
 print('-='*30)
-print(f'{"Nº.":<4}{"NAME":<10}{"MÉDIA":>8.1f}')
+print(f'{"Nº.":<4}{"NOME":<10}{"MÉDIA":>8}')
 print('-'*26)
 
 for i, a in enumerate(record):
-    print(f'{i:<4}{a[0]<10}{a[2]:>8.1f}')
+    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
 while True:
     print('-'*35)
     opc = int(input('Mostrar notas de qual aluno? (Pressione 999 para sair ): '))
     if opc == 999:
         break
     if opc <= len(record) - 1:
-        print(f'Notas de {record[opc][0]} são {record[opc][1]}')
+        print(f'Notas de {record[opc][0]} foram: {record[opc][1]}')
