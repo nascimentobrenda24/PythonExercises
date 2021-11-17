4a7ca8f8030657f5b36a29daaae0e595ab4921b3
Brenda Nascimento <74272215+nascimentobrenda24@users.noreply.github.com>
2021-11-17 17:46:58 -0300

    Update ex089.py

--- ex089.py
+++ ex089.py
@@ -5,15 +5,15 @@
 record = []
 
 while True:
-    print('='*15)
+    print('=' * 15)
     name = str(input('Nome: '))
 
-    grade1 = float(input('Nota 1: '))
-    grades2 = float(input('Nota 2: '))
+    grade1 = float(input('Nota1: '))
+    grade2 = float(input('Nota2: '))
 
-    average = (grade1 + grades2) / 2
+    average = (grade1 + grade2) / 2
 
-    record.append([name, [grade1, grades2], average])
+    record.append([name, [grade1, grade2], average])
 
     answer = str(input('Quer continuar? [S/N]: ')).upper()[0]
     if answer in 'Nn':
@@ -22,13 +22,10 @@ while True:
 print('-='*30)
 print(f'{"Nº.":<4}{"NOME":<10}{"MÉDIA":>8}')
 print('-'*26)
-
-for i, a in enumerate(record):
-    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
 while True:
-    print('-'*35)
-    opc = int(input('Mostrar notas de qual aluno? (Pressione 999 para sair ): '))
+    opc = int(input('Mostrar notas de qual aluno? (Pressione 999 para sair): '))
     if opc == 999:
         break
     if opc <= len(record) - 1:
-        print(f'Notas de {record[opc][0]} foram: {record[opc][1]}')
+        print(f'As notas de {record[opc][0]} foram: {record[opc][1]}')
+print('<<<VOLTE SEMPRE>>>')
