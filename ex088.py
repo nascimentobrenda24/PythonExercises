83b5bdf75e0af602b11ea291396d7e01f3b128de
Brenda Nascimento <74272215+nascimentobrenda24@users.noreply.github.com>
2021-11-17 17:09:37 -0300

    Update ex088.py

--- ex088.py
+++ ex088.py
@@ -1,26 +1,39 @@
 # Faça um programa que ajude um jogador da MEGA SENA a criar palpites. O programa vai perguntar quantos jogos serão
 # gerados e vai sortear 6 números entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta.
-
 from random import randint
-list = list()
+
 games = []
+temp = []
+
 print('-'*30)
-print('    JOGA NA MEGA SENA   ')
+print('        JOGA NA MEGA SENA      ')
 print('-'*30)
-answer = int(input('Quantos jogos você quer que eu sortei?: '))
-tot_games = 1
-while tot_games <= answer:
+
+q = int(input('Quantos jogos você quer que eu sortei? '))
+tot = 1
+
+while tot <= q:
     cont = 0
     while True:
         num = randint(1, 60)
-        if num not in list:
-            list.append(num)
-            cont += 1
-        if cont >= 6:
-            break
 
-    list.sort()
-    games.append(list[:])
-    list.clear()
-    tot_games += 1
-    print(f'Os números sorteados foram {games}')
+        if num not in temp:
+            temp.append(num)
+            cont += 1
+
+            if cont >= 6:
+                break
+
+    temp.sort()
+    games.append(temp[:])
+    temp.clear()
+    tot += 1
+
+print(f'=-'*3, f'SORTEANDO {q} JOGOS', '-='*3)
+for i, l in enumerate(games):
+    print(f'Jogo {i+1}: {l}')
+
+print('-='*5, 'BOA SORTE', '-='*5)
+
+
+
