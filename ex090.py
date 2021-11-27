5f472846431d193699f34bf5f7bb1cbe7eb82d77
Brenda Nascimento <74272215+nascimentobrenda24@users.noreply.github.com>
2021-11-27 12:00:40 -0300

    Update ex090.py

--- ex090.py
+++ ex090.py
@@ -1,3 +1,19 @@
 # Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário. No final, mostre
 # o conteúdo da estrutura na tela.
 
+name = str(input('Nome do Aluno(a): '))
+average = float(input('Média: '))
+
+# Adding in dictionary
+total = {"Nome": name, "Média": average}
+
+# Calculating and comparing the average
+acceptance_criteria = average >= 7.5
+print('=*'*30)
+for n, a in total.items():
+    print(f'{n} é igual {a}')
+
+if average != acceptance_criteria:
+    print('Situação é igual Reprovado(a) ')
+else:
+    print('Situação é igual Aprovado(a)')
