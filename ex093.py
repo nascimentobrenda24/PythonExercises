8e1282083fd0ed77612104098f81af4292ccc280
Brenda Nascimento <74272215+nascimentobrenda24@users.noreply.github.com>
2021-11-30 14:02:14 -0300

    Update ex093.py

--- ex093.py
+++ ex093.py
@@ -2,9 +2,10 @@
 # e quantas partidas ele jogou. Depois vai ler a quantidade de gols feitos em cada partida. No final, tudo isso será
 # guardado em um dicionário, incluindo o total de gols feitos durante o campeonato.
 
+# ** I DID ALONE THIS EXERCISE
 game = {}
+each_matches = []
 tot = 0
-list_goals = []
 
 game["Nome"] = str(input("Nome do Jogador: "))
 game["Partidas"] = int(input(f'Quantas Partidas {game["Nome"]} jogou?: '))
@@ -12,13 +13,25 @@ game["Partidas"] = int(input(f'Quantas Partidas {game["Nome"]} jogou?: '))
 if game["Partidas"] != 0:
     # Calculating the goals in the respective games in time of matches, whose this player played
     for p in range(game["Partidas"]):
-        goals_per_matches = [int(input(f'  - Gols na partida {p+1}: '))]
-        #list_goals = goals_per_matches
+        goals_per_matches = int(input(f'  - Gols na partida {p+1}: '))
+        each_matches.append(goals_per_matches)
 
-        for t, p in enumerate(goals_per_matches):
-            tot += goals_per_matches[p]
-            game["Gols por Partida"] = goals_per_matches
+        tot += goals_per_matches
+        game["Gols por Partida"] = each_matches
         game["Total de Gols"] = tot
+else:
+    print('Esse jogador(a) não participou de nenhuma partida.')
 
-
+print('-='*30)
+# Iº demonstration
 print(game)
+# IIº demonstration
+print('-='*30)
+for k, v in game.items():
+    print(f'O campo {k} está preenchido com {v}')
+# IIIº demonstration
+print('-='*30)
+print(f'O jogador(a) {game["Nome"]} jogou {game["Partidas"]} partidas')
+for p in range(game["Partidas"]):
+    print(f'Na partida {p+1}, fez {game["Gols por Partida"][p]} gols')
+
