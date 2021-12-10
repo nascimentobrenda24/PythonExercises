a700e8b1a16424fab08a6c02e0a49361bc11c6c7
Brenda Nascimento <74272215+nascimentobrenda24@users.noreply.github.com>
2021-12-10 08:51:01 -0300

    Update ex094.py

--- ex094.py
+++ ex094.py
@@ -8,20 +8,24 @@
 
 guys = list()
 person = dict()
-sum = average = 0
-cont = 0
+
+sum_year = average = cont = 0
 
 while True:
     person.clear()
     person["nome"] = str(input('Nome: '))
+
     while True:
         person["sexo"] = str(input('Sexo [M/F]: ')).upper()[0]
         if person["sexo"] in 'MF':
             break
         print('ERRO! Por favor, digite apenas M ou F.')
+
     person["idade"] = int(input('Idade: '))
-    sum += person["idade"]
+    sum_year += person["idade"]
+
     guys.append(person.copy())
+
     while True:
         proceed = str(input('Quer continuar [S/N]: ').upper())[0]
         if proceed in 'SN':
@@ -31,18 +35,18 @@ while True:
 
 print('-='*30)
 # A
-print(f'A) Ao todo temos {len(guys)} cadastradas')
+print(f'A) Ao todo temos {len(guys)} pessoa(s) cadastrada(s)')
 # B
-average = sum / len(guys)
-print(f'B) A médida de idade é de de {average} anos')
+average = sum_year / len(guys)
+print(f'B) A médida de idade é de {average:.2f} anos')
 # C
-print('C) As mulheres cadastradas foram: ', end='')
+print('C) Lista da(s) mulher(es) cadastrada(s): ', end='')
 for p in guys:
     if p["sexo"] in 'Ff':
         cont += 1
-        print(f'{p["nome"]}', end='')
+        print(f'{p["nome"]}', end=',')
 # D
-print(f'\nD) Lista da(s) pessoa(s) que estão acima da média', end='')
+print(f'\nD) Lista da(s) pessoa(s) que estão acima da média: ', end='')
 for p in guys:
     if p["idade"] >= average:
         print('       ', end='')
